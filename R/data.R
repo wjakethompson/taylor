764a37de29860d965a9f6dcf6c21d3f92a7868b6
Jake Thompson <wjakethompson@gmail.com>
2021-06-27 21:47:30 -0500

    start documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Lyrics to Taylor Swift Songs
+#'
+#' A data set containing lyrics to all officially released Taylor Swift songs.
+#'
+#' @format A data frame with XX rows and YY variables:
+#'
+#'
+#' @details
+#' Lyrics come from
+#'
+#'
+#' @source \url{https://genius.com/artists/Taylor-swift}
+"taylor_lyrics"
