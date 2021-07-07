14ac7e387db04a1f92b99feb2fa4c46645f9722b
Jake Thompson <wjakethompson@gmail.com>
2021-07-07 15:19:53 -0500

    use tibble correctly

--- R/taylor-package.R
+++ R/taylor-package.R
@@ -21,5 +21,6 @@ NULL
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
