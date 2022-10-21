70d314a62a979eb58f69cc225657411e4600951c
Jake Thompson <wjakethompson@gmail.com>
2022-10-21 13:16:14 -0500

    combine docs for all songs and album songs

--- R/data.R
+++ R/data.R
@@ -1,11 +1,53 @@
 #' Data for Taylor Swift songs
 #'
-#' A data set containing lyrics to and characteristics of all officially
+#' A data containing lyrics to and characteristics of all officially
 #' released Taylor Swift songs. This includes albums, EPs, and individually
 #' released singles.
 #'
-#' @format A [tibble][tibble::tibble-package] with `r nrow(taylor_all_songs)`
-#' rows and `r ncol(taylor_all_songs)` variables. Each row is one song.
+#' @details
+#' `taylor_all_songs` contains all songs in Taylor's discography.
+#' Lyrics come from Genius, and songs characteristics come from the Spotify API.
+#' Some data is known to be missing. The Beautiful Eyes EP is not available on
+#' any streaming service, and therefore has no data from the Spotify API.
+#' Similarly, the song *American Girl*, a cover of the Tom Petty original, was
+#' released exclusively on Rhapsody (now Napster), and therefore also does not
+#' have data from the Spotify API.
+#'
+#' For songs released separately from Taylor's official albums or EPs, album
+#' information is not included. For example, *I Don't Wanna Live Forever* was
+#' released as part of the *Fifty Shades Darker* movie soundtrack. However, the
+#' `album_name` column for this song is `NA`, indicating that it does not appear
+#' on one of Taylor's albums.
+#'
+#' Songs are only included one time. For example, if a song appears on both the
+#' standard and deluxe version of an album, there is only one record of the
+#' song in the data set. Similarly, compilations are not included. For example,
+#' following the release of *folklore*, Taylor released several EPs that were
+#' subsets of the original *folklore* album (e.g., *folklore: the escapism
+#' chapter*, *folklore: the sleepless nights chapter*, etc.). These are not
+#' included. Finally, all bonus tracks (with the exception of voice
+#' memos or similar) are included; however, for consistency, the album name is
+#' always the shortened, common name. For example, *the lakes* is a bonus track
+#' from *folklore (deluxe edition)*, but the `album_name` is listed only as
+#' *folklore*. The `bonus_track` variable can be used to determine which songs
+#' appeared on the standard version of an album vs. a deluxe or platinum
+#' edition.
+#'
+#' `taylor_album_songs` contains the same information as `taylor_all_songs`, but
+#' only for Taylor's official studio albums. Thus, `taylor_album_songs` is a
+#' subset of [`taylor_all_songs`], with EPs and individual singles excluded.
+#' Critically, this subset also only includes versions owned by Taylor when
+#' possible, because we stan artists owning their own work. This means that
+#' although both *Fearless* and *Fearless (Taylor's Version)* appear in the full
+#' [`taylor_all_songs`] data, only *Fearless (Taylor's Version)* appears in this
+#' subset. This also means that this data set will change as additional
+#' re-releases are made available (i.e., *Red* was replaced with
+#' *Red (Taylor's Version)* in [version
+#' 1.0.0](https://wjakethompson.com/blog/taylor/2021-12-17-taylor-1.0.0/)).
+#'
+#' @format `taylor_all_songs` is a [tibble][tibble::tibble-package] with
+#' `r nrow(taylor_all_songs)` rows and `r ncol(taylor_all_songs)` variables.
+#' Each row is one song.
 #' * `album_name`: The name of the album. `NA` if the song was released
 #'   separately from one of Taylor's studio albums or EPs.
 #' * `ep`: Logical. Is the album a full studio album (`FALSE`) or an extended
@@ -77,55 +119,16 @@
 #'     `Taylor Swift`, but other artists appear if they are featured on the
 #'     track (e.g., `HAIM` is featured on *No Body, No Crime*).
 #'
-#' @details
-#' Lyrics come from Genius, and songs characteristics come from the Spotify API.
-#' Some data is known to be missing. The Beautiful Eyes EP is not available on
-#' any streaming service, and therefore has no data from the Spotify API.
-#' Similarly, the song *American Girl*, a cover of the Tom Petty original, was
-#' released exclusively on Rhapsody (now Napster), and therefore also does not
-#' have data from the Spotify API.
-#'
-#' For songs released separately from Taylor's official albums or EPs, album
-#' information is not included. For example, *I Don't Wanna Live Forever* was
-#' released as part of the *Fifty Shades Darker* movie soundtrack. However, the
-#' `album_name` column for this song is `NA`, indicating that it does not appear
-#' on one of Taylor's albums.
-#'
-#' Songs are only included one time. For example, if a song appears on both the
-#' standard and deluxe version of an album, there is only one record of the
-#' song in the data set. Similarly, compilations are not included. For example,
-#' following the release of *folklore*, Taylor released several EPs that were
-#' subsets of the original *folklore* album (e.g., *folklore: the escapism
-#' chapter*, *folklore: the sleepless nights chapter*, etc.). These are not
-#' included. Finally, all bonus tracks (with the exception of voice
-#' memos or similar) are included; however, for consistency, the album name is
-#' always the shortened, common name. For example, *the lakes* is a bonus track
-#' from *folklore (deluxe edition)*, but the `album_name` is listed only as
-#' *folklore*. The `bonus_track` variable can be used to determine which songs
-#' appeared on the standard version of an album vs. a deluxe or platinum
-#' edition.
-#'
 #' @source \url{https://genius.com/artists/Taylor-swift}
 #' @source \url{https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02}
+#' @rdname taylor_songs
 "taylor_all_songs"
 
-#' Data for songs on Taylor Swift's official studio albums
-#'
-#' A data set containing lyrics to and characteristics of songs on all of
-#' Taylor's official studio albums. Thus, this is a subset of
-#' [`taylor_all_songs`], with EPs and individual singles excluded. Critically,
-#' this subset also only includes versions owned by Taylor when possible,
-#' because we stan artists owning their own work. This means that although both
-#' *Fearless* and *Fearless (Taylor's Version)* appear in full
-#' [`taylor_all_songs`] data, only *Fearless (Taylor's Version)* appears in this
-#' subset. This also means that this data set will change as additional
-#' re-releases are made available (i.e., *Red* will soon be replaced with
-#' *Red (Taylor's Version)*).
-#'
-#' @format A [tibble][tibble::tibble-package] with `r nrow(taylor_album_songs)`
-#' rows and `r ncol(taylor_album_songs)` variables. This is a subset of the
-#' [`taylor_all_songs`] data set. Please see that documentation for a complete
-#' description of all the included fields.
+#' @format `taylor_album_songs` is a [tibble][tibble::tibble-package] containing
+#' the same `r ncol(taylor_album_songs)` variables, but only with
+#' `r nrow(taylor_album_songs)` rows, one for each song from an official studio
+#' album (see Details).
+#' @rdname taylor_songs
 "taylor_album_songs"
 
 #' Data for Taylor Swift's studio albums and EPs
