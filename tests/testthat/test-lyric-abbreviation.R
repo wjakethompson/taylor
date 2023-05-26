49fa04619e666636444e6cd7498ff6437d332d18
Jake Thompson <wjakethompson@gmail.com>
2023-05-26 13:08:58 -0500

    update tests

--- tests/testthat/test-lyric-abbreviation.R
+++ tests/testthat/test-lyric-abbreviation.R
@@ -9,7 +9,15 @@ test_that("translation works", {
   expect_equal(translate_bracelet("bykmosftvfw"),
                glue::glue_data(atw, "{track}: {line}\n\n"))
 
+  krm <- tibble::tibble(
+    track = c("Karma", "Karma (Remix)"),
+    line = paste0("karma is my boyfriend Karma is a god Karma is the breeze ",
+                  "in my hair on the weekend")
+  )
   expect_equal(translate_bracelet("kimbkiagkitbimhotw"),
-               glue::glue("Karma: karma is my boyfriend Karma is a god Karma ",
-                          "is the breeze in my hair on the weekend\n\n"))
+               glue::glue_data(krm, "{track}: {line}\n\n"))
+
+  expect_equal(translate_bracelet("ymtotcd"),
+               glue::glue("champagne problems: Your Midas touch on the ",
+                          "Chevy door\n\n"))
 })
