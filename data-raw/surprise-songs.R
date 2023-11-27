9518f456e362e585bad909215c5ce1cf1722e810
Jake Thompson <wjakethompson@gmail.com>
2023-11-26 21:16:15 -0600

    resolve lints

--- data-raw/surprise-songs.R
+++ data-raw/surprise-songs.R
@@ -17,17 +17,17 @@ eras_tour_surprise <- read_xlsx(here("data-raw", "surprise-songs.xlsx")) %>%
 
 # Check that we're using Taylor's Version when possible. Should be 0 rows.
 (not_tv <- eras_tour_surprise %>%
-    filter(!str_detect(song, fixed("Taylor's Version"))) %>%
-    mutate(tv = map(song,
-                    \(x) {
-                      taylor_all_songs %>%
-                        filter(str_detect(track_name, x)) %>%
-                        filter(str_detect(track_name,
-                                          fixed("Taylor's Version"))) %>%
-                        select(album_name, track_name)
-                    })) %>%
-    filter(map_lgl(tv, \(x) nrow(x) > 0)) %>%
-    select(song, tv) %>%
-    unnest(tv))
+  filter(!str_detect(song, fixed("Taylor's Version"))) %>%
+  mutate(tv = map(song,
+                  \(x) {
+                    taylor_all_songs %>%
+                      filter(str_detect(track_name, x)) %>%
+                      filter(str_detect(track_name,
+                                        fixed("Taylor's Version"))) %>%
+                      select(album_name, track_name)
+                  })) %>%
+  filter(map_lgl(tv, \(x) nrow(x) > 0)) %>%
+  select(song, tv) %>%
+  unnest(tv))
 
 use_data(eras_tour_surprise, overwrite = TRUE)
