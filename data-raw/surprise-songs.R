ac5620998ec2fc34a28f17b88383c6e338a7847a
Jake Thompson <wjakethompson@gmail.com>
2023-11-26 21:01:13 -0600

    finish south american surprise songs

--- data-raw/surprise-songs.R
+++ data-raw/surprise-songs.R
@@ -7,7 +7,7 @@ devtools::load_all()
 eras_tour_surprise <- read_xlsx(here("data-raw", "surprise-songs.xlsx")) %>%
   mutate(date = as_date(date),
          night = as.integer(night)) %>%
-  filter(date < today())
+  filter(date <= today())
 
 
 # QC data file -----------------------------------------------------------------
@@ -17,13 +17,17 @@ eras_tour_surprise <- read_xlsx(here("data-raw", "surprise-songs.xlsx")) %>%
 
 # Check that we're using Taylor's Version when possible. Should be 0 rows.
 (not_tv <- eras_tour_surprise %>%
-   left_join(taylor_all_songs %>%
-               filter(is.na(ep) | !ep) %>%
-               distinct(track_name, album_name),
-             join_by(song == track_name),
-             relationship = "many-to-one") %>%
-   filter(!is.na(album_name),
-          !album_name %in% taylor_album_songs$album_name) %>%
-   distinct(song, album_name))
+    filter(!str_detect(song, fixed("Taylor's Version"))) %>%
+    mutate(tv = map(song,
+                    \(x) {
+                      taylor_all_songs %>%
+                        filter(str_detect(track_name, x)) %>%
+                        filter(str_detect(track_name,
+                                          fixed("Taylor's Version"))) %>%
+                        select(album_name, track_name)
+                    })) %>%
+    filter(map_lgl(tv, \(x) nrow(x) > 0)) %>%
+    select(song, tv) %>%
+    unnest(tv))
 
 use_data(eras_tour_surprise, overwrite = TRUE)
