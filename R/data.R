ec264310afc1f985e64d44886d04368a986d13b0
Jake Thompson <wjakethompson@gmail.com>
2024-02-18 15:57:20 -0600

    add mashups to surprise song data

--- R/data.R
+++ R/data.R
@@ -171,6 +171,7 @@
 #' * `night`: The show number within each city.
 #' * `dress`: The color of the dress Taylor wore on the given night.
 #' * `instrument`: The instrument used to play the song (guitar or piano).
-#' * `song`: The track name of the song.
+#' * `song`: The track name of the primary surprise song.
+#' * `mashup`: Additional songs included in a mashup with the primary song.
 #' * `guest`: The special guest (if any) that joined Taylor to play the song.
 "eras_tour_surprise"
