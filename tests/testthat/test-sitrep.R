48655759744e9a129f6bb2af7d32d01954c38339
Jake Thompson <wjakethompson@gmail.com>
2026-02-05 11:33:12 -0600

    update tests

--- tests/testthat/test-sitrep.R
+++ tests/testthat/test-sitrep.R
@@ -1,9 +1,15 @@
 test_that("sitrep works", {
+  withr::local_envvar(list(
+    "SPOTIFY_CLIENT_ID" = spotify_testing_key()$client_id,
+    "SPOTIFY_CLIENT_SECRET" = spotify_testing_key()$client_secret,
+    "SOUNDSTAT_KEY" = soundstat_testing_key()
+  ))
   expect_snapshot(taylor_sitrep())
 
   withr::local_envvar(list(
     "SPOTIFY_CLIENT_ID" = "",
-    "SPOTIFY_CLIENT_SECRET" = ""
+    "SPOTIFY_CLIENT_SECRET" = "",
+    "SOUNDSTAT_KEY" = soundstat_testing_key()
   ))
   expect_snapshot(taylor_sitrep())
 })
